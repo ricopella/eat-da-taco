1cdf534c374dcf0873015f71e1e73311dc3f57e4
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-11 19:54:20 -0700

    setup orm

---
+++ config/orm.js
@@ -0,0 +1,50 @@
+const connection = require("../config/connection");
+
+const orm = {
+    selecAll: function(tableInput, cb) {
+        let queryString = `SELECT * FROM ` + tabelInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
