a04a5e857e77c7f811ec53865aea91b85a46f83e
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-11 21:12:29 -0700

    updates to ORM, Controller, and Views

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,32 @@
 const connection = require("../config/connection");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        if (Object.hasOwnProperty.call(ob, key)) {
+            arr.push(key + "=" + ob[key]);
+        }
+    }
+
+    return arr.toString();
+}
+
 const orm = {
     selectAll: function(tableInput, cb) {
-        let queryString = `SELECT * FROM ` + tableInput + ";";
+        let queryString = `SELECT * FROM ` + tableInput + ` WHERE devoured = 0` + ";";
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
@@ -15,10 +39,10 @@ const orm = {
 
         queryString += " (";
         queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+        queryString += ", devoured) ";
+        queryString += "VALUES ('";
+        queryString += vals.toString();
+        queryString += "', false) ";
 
         console.log(queryString);
 
