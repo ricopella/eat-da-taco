cf6308d6045c6ffe8bda91c467d4b062b01871f4
Narin Sundarabhaya <nrs710@gmail.com>
2017-08-13 14:52:19 -0700

    removed method-overide

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
+// const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers/taco_controller.js");
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// // Override with POST having ?_method=DELETE
+// app.use(methodOverride("_method"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
